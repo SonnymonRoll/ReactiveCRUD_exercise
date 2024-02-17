54c32df6e54a6f52fa490fac1e0e358cd8b08b2c
Sonia Sofía García Gómez <sonia.garcia@optimen.com.mx>
2024-02-16 22:27:39 -0600

    Reactive template

---
+++ src/test/java/com/reactivecrud/api/ReactiveCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivecrud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
